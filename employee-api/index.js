40489fc63d299b9245fb3cdba5d2a0a6f46a59ce
dharma-byte <puttadharmateja6@gmail.com>
2025-03-24 10:49:44 +0530

    Port number

--- employee-api/index.js
+++ employee-api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.use(bodyParser.json());
 
