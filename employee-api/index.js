51bde54ad4ac45a431f57acb3136bba48b4e4d96
dharma-byte <puttadharmateja6@gmail.com>
2025-03-24 11:58:16 +0530

    Port address updated

--- employee-api/index.js
+++ employee-api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
 const app = express();
-const PORT = 8000;
+const PORT = 7000;
 
 app.use(bodyParser.json());
 
