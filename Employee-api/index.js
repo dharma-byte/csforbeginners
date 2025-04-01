1527e4fcd1772a5bf8322104b7ab451f6dc5350f
Dharma Teja <puttadharmateja6@gmail.com>
2025-04-01 20:10:57 +0530

    port number

--- Employee-api/index.js
+++ Employee-api/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./db');
 const app = express();
-const PORT = 7000;
+const PORT = 2000;
 
 app.use(bodyParser.json());
 
