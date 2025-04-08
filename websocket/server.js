0a6cd550b833e500ef9dfed91cd3336a5994872c
Dharma Teja <puttadharmateja6@gmail.com>
2025-04-08 15:19:39 +0530

    count 1 to 100 numbers

--- websocket/server.js
+++ websocket/server.js
@@ -1,17 +1,26 @@
-// server.js
 const WebSocket = require('ws');
 
-const server = new WebSocket.Server({ port: 8080 });
-console.log("WebSocket server running on ws://localhost:8080");
+const server = new WebSocket.Server({ port: 3000 });
+console.log("WebSocket server started at ws://localhost:3000");
 
 server.on('connection', socket => {
-    console.log('Client connected');
+    console.log("Client connected");
 
-    socket.on('message', message => {
-        console.log(`Received:`, message);
-        const customResponse = "Got your message!";
-        socket.send(customResponse);
+    let count = 1;
+    const interval = setInterval(() => {
+        if (count <= 100) {
+            socket.send(count.toString());
+            console.log("Sent:", count);
+            count++;
+        } else {
+            clearInterval(interval);
+            socket.send("Done!");
+            socket.close();
+        }
+    }, 1000); // 1000 ms = 1 second
+
+    socket.on('close', () => {
+        console.log("Client disconnected");
+        clearInterval(interval); // Cleanup if client disconnects early
     });
-
-    socket.on('close', () => console.log('Client disconnected'));
 });
